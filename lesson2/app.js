58606b8d8dbc561f0a211456ba41e207aaf8b2eb
“determinate1209“ <determinate1209@gmail.com>
2022-07-07 16:28:19 +0900

    javascript学習

---
+++ lesson2/app.js
@@ -0,0 +1,12 @@
+let num1 = '1+1='
+let num2 = 1+1
+
+let num3 = '2-1='
+let num4 = 2-1
+
+let num5 = '2X3='
+let num6 = 2*3
+
+let num7 = '4/2='
+let num8 = 4/2
+alert(num1+num2+'\n'+num3+num4+'\n'+num5+num6+'\n'+num7+num8);
