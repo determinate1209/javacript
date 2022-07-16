af5ca244aeffabf176a74e98721b10e82a36aea9
“determinate1209“ <determinate1209@gmail.com>
2022-07-16 14:34:59 +0900

    skippr導入

---
+++ lesson8/js/script.js
@@ -0,0 +1,14 @@
+// オプションを指定してSkipprの実行
+$(".theTarget").skippr({
+
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navType : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : true,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
